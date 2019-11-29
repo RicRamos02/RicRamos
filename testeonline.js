b51c5cbcdc27ca6dbc2dbc1f8838c8188a34ad40
RicRamos02 <ricardo.ramos17@hotmail.com>
2019-11-29 15:23:37 +0000

    tcjct

--- testeonline.js
+++ testeonline.js
@@ -87,8 +87,8 @@ function printboard(){
 		let z = tabul[i][0];
 		let w = tabul[i][1];
 		let cod = 127025 + z * 7 + w;
-		if (!isNaN){
-			boas2.innerHTML = "&#" + cod;
+		if (!isNaN(cod)){
+			boas2.innerHTML = "&#" + this.cod;
 			document.getElementById("Board2").appendChild(boas2);
 		}
 	}
@@ -124,6 +124,177 @@ function dir() {
 
 
 
+lines = 1;
+function ranking() {
+	var a = document.getElementById("tabela");
+	fetch ('http://twserver.alunos.dcc.fc.up.pt:8008/ranking', {
+	method:'POST',
+	body: JSON.stringify(a)
+})
+.then(response => {
+	return response.json();
+})
+.then(function(response) {
+	console.log(response);
+	if (response.error==null) {
+		var table = document.getElementById("tabela");
+		for (var i=0; i<response.ranking.length;i++) {
+			var server_data = table.insertRow(lines);
+			var cell1 = server_data.insertCell(0);
+			var cell2 = server_data.insertCell(1);
+			var cell3 = server_data.insertCell(2);
+			cell1.innerHTML = response.ranking[i].nick;
+			cell2.innerHTML = response.ranking[i].victories;
+			cell3.innerHTML = response.ranking[i].games;
+			lines++;
+		}
+	}
+})
+}
 
+function registo () {
+	var pass = document.getElementById("pass").value;
+	nome = document.getElementById("user").value;
+	if((nome!="")&&(pass!="")){
+		var x = JSON.stringify({nick:nome,pass:pass});
+		fetch('http://twserver.alunos.dcc.fc.up.pt:8008/register', {
+		method:'POST',
+		body:x
+	})
+	.then(response =>{return response.json();
+	})
+	.then(function(response) {
+		if(response.error!=null)
+		alert("User registered with a different password");
+		else{
+			loginFunc();
+			getUser();
+		}
+	})
+}
+}
+
+function join(){
+	limpar();
+	var group = 12;
+	var pass = document.getElementById("pass").value;
+	if ((nome!="")&&(pass!="")){
+		var x = JSON.stringify({group:group,nick:nome,pass:pass});
+		fetch('http://twserver.alunos.dcc.fc.up.pt:8008/join', {
+		method:'POST',
+		body:x
+	})
+	.then(response=>{console.log(response);return response.json();
+	})
+	.then(function(response) {
+		if(response.error!=null)
+			alert("Pairing error");
+		else {
+			console.log("tou aqui");
+			disappear2();
+			start2();
+			for (let j=0; j<response.hand.length; j++){
+				mypieceson[j][0] = response.hand[j][0];
+				mypieceson[j][1] = response.hand[j][1];
+			}
+			game_id = response.game;
+			maxonline(mypieceson);
+			update();
+			tabuleiro_on();
+			alert("coisas");
+		}
+	})
+}
+}
+
+function notify(){
+	var pass = document.getElementById("pass").value;
+	if (piece == null ) {
+		var x = JSON.stringify({nick:nome,pass:pass,game:game_id,piece:null});
+		fetch('http://twserver.alunos.dcc.fc.up.pt:8008/notify', {
+			method: 'POST',
+			body: x
+		})
+			.then(response => {
+				console.log(response); return response.json();
+			})
+			.then(data => {
+				
+				console.log("data", data);
+			})
+			/*.then(function (response) {
+			
+				alert("pedi peca");
+				printboard();
+			})*/
+	}
+	else {
+		console.log(piece);
+		var x = JSON.stringify({ nick: nome, pass: pass, game: game_id, piece: piece, side: side })
+		fetch('http://twserver.alunos.dcc.fc.up.pt:8008/notify', {
+			method: 'POST',
+			body: x
+		})
+			.then(response => {
+				console.log(response); return response.json();
+			})
+			.then(function (response) {
+				if (response.side != null) {
+					document.getElementById("warnings2").innerHTML = escolhelado;
+					document.getElementById("esq").style.display = "block";
+					document.getElementById("dir").style.display = "block";
+				}
+				alert("tentei jogar");
+				printboard();
+			})
+	}
+}
+
+function update(){
+	if(estado == "inicia"){
+		evtSource = new EventSource("http://twserver.alunos.dcc.fc.up.pt:8008/update?nick=" + nome + "&game=" + game_id);
+		evtSource.onmessage = function(event){
+			const data1 = JSON.parse(event.data);
+			if(data1.turn == nome){
+				for (let i=0; i<data1.board.line.length; i++){
+					tabul[i][0] = data1.board.line[i][0];
+					tabul[i][1] = data1.board.line[i][1];
+				}
+				printboard();
+				if (num==1)
+					document.getElementById("warnings2").innerHTML = pecamaior;
+				else
+					document.getElementById("warnings2").innerHTML = tuavez;
+			}
+			else if (data1.turn != nome)
+				document.getElementById("warnings2").innerHTML = espera;
+			if(data1.winner != null){
+				alert(data1.winner + " ganhou!");
+				estado = "acaba";
+				update();
+			}
+		}
+	}
+	else {
+		evtSource.close();
+	}
+}
+
+function desistir(){
+	var pass = document.getElementById("pass").value;
+	var url = "http://twserver.alunos.dcc.fc.up.pt:8008/leave";
+	var desiste = JSON.stringify({game:game_id , nick:nome , pass:pass});
+	fetch(url,{method:'POST',body:desiste}).
+	then(response=>{
+		return response.json();
+	}).
+	then(function(data) {
+		if(data.error==null){
+			game_id=0;
+			estado = "acaba";
+			update();
+		}
+	})
+}
 
 
